cf94864a275ea61f253170feab62adf0ff3533ba
felixpalazuelos <felixpalazuelos@gmail.com>
2014-02-11 10:55:55 +0100

    git alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,6 +12,36 @@ $env:TERM = "msys"
 Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
 Set-Alias vim "C:\Program Files (x86)\Vim\vim74\vim.exe"
 
+#remove default alias
+rename-item alias:\gc gcc -force
+rename-item alias:\gp gpp -force
+rename-item alias:\gl gll -force
+
+# Git Aliases
+function git-status { git status }
+Set-Alias -Name gs -Value git-status
+
+function git-push { git status }
+Set-Alias -Name gp -Value git-push
+
+function git-pull { git pull }
+Set-Alias -Name gl -Value git-pull
+
+function git-add-update { git add -u}
+Set-Alias -Name gau -Value git-add-update
+
+function git-commit-m { git commit -m $args }
+Set-Alias -Name gc -Value git-commit-m
+
+function git-diff { git diff }
+Set-Alias -Name gd -Value git-diff
+
+function git-checkout { git checkout }
+Set-Alias -Name gco -Value git-checkout
+
+function git-log { git log }
+Set-Alias -Name glog -Value git-log
+
 # Load posh-git example profile
 . 'C:\Users\felixpalazuelos\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
